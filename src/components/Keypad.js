2f1a79663a12ba63d4122015b442e06e527017e4
Timothy Ryon <tim@timryon.com>
2024-01-21 21:01:30 -0500

    finished assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
-// Code Keypad Component Here
+import React, { useState } from "react";
 
 function Keypad (){
+
+
+    function handlePassword() {
+        console.log('Entering password...')
+    }
+
     return (
-        <div></div>
+        <input type="password" onChange = {handlePassword } />
     )
 }
 
